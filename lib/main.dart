db729569665be47c894b3fff7a0e5b38450e3ff6
Romeo <romeo.enso93@gmail.com>
2019-11-07 14:14:36 +0800

    added readme

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
@@ -37,51 +38,50 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5
                 ),
               ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                padding: EdgeInsets.all(10),
-                color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      '+639 21524 8318',
-                      style: TextStyle(
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                          color: Colors.teal.shade900,
-                          fontWeight: FontWeight.bold,
-                      ),
-                    )
-                  ],
-                ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                    color: Colors.teal.shade100
+                )
               ),
-              Container(
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                padding: EdgeInsets.all(10),
                 color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      'romeo.enso93@gmail.com',
-                      style: TextStyle(
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                          color: Colors.teal.shade900,
-                          fontWeight: FontWeight.bold,
-                      ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+639 21524 8318',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontWeight: FontWeight.bold,
                     )
-                  ],
-                ),
+                  ),
+                )
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ) ,
+                  title: Text(
+                    'romeo.enso93@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                )
               )
             ],
           )
